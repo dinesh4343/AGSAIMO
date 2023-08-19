2ed2c4f7180cee0d90fdd8dfdea0787045eeb5e8
DINESH <109014495+dinesh4343@users.noreply.github.com>
2023-08-19 19:16:30 +0530

    push

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Navbar
+
 const hamburger = document.querySelector('.hamburger');
 const navButtonsMobile = document.querySelector('.nav-buttons-mobile');
 const lines = document.querySelectorAll('.line');
@@ -13,49 +13,26 @@ hamburger.addEventListener('click', () => {
 
 
 
-var popupDialog = document.getElementById('popupDialog');
-var closeButton = document.getElementById('closeButton');
 
-function openPopup() {
-    // Show the popup dialog with a slow opening effect
-    popupDialog.style.display = 'flex';
-    popupDialog.style.opacity = '0';
-    var opacity = 0;
-    var interval = setInterval(function () {
-        opacity += 0.1;
-        popupDialog.style.opacity = opacity;
-        if (opacity >= 1) {
-            clearInterval(interval);
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var backToTopBtn = document.getElementById("back-to-top-btn");
+    
+
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 300) { 
+            backToTopBtn.style.display = "block";
+        } else {
+            backToTopBtn.style.display = "none";
         }
-    }, 50);
-    popupDialog.style.pointerEvents = 'auto';
-}
-
-function closePopup() {
-    // Close the popup dialog with a smooth closing effect
-    var opacity = 1;
-    var interval = setInterval(function () {
-        opacity -= 0.1;
-        popupDialog.style.opacity = opacity;
-        if (opacity <= 0) {
-            clearInterval(interval);
-            popupDialog.style.display = 'none';
-            popupDialog.style.pointerEvents = 'none';
-        }
-    }, 50);
-}
-
-document.getElementById('learnMore').addEventListener('click', function () {
-    openPopup();
-});
-
-closeButton.addEventListener('click', function () {
-    closePopup();
-});
-
-window.addEventListener('click', function (event) {
-    // Close the popup dialog if clicked outside of it with a smooth transition
-    if (event.target == popupDialog) {
-        closePopup();
-    }
+    });
+    
+   
+    backToTopBtn.addEventListener("click", function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
 });
