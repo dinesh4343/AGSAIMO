5a392b30f0675f68935d7b0167b1cb9e066d3392
DINESH <109014495+dinesh4343@users.noreply.github.com>
2023-08-19 13:56:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('.hamburger');
+const navButtonsMobile = document.querySelector('.nav-buttons-mobile');
+const lines = document.querySelectorAll('.line');
+
+hamburger.addEventListener('click', () => {
+    navButtonsMobile.classList.toggle('hidden-nav');
+    lines[0].classList.toggle('close-1');
+    lines[1].classList.toggle('close-2');
+    lines[2].classList.toggle('close-3');
+});
